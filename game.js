2f4c668b0fee536aa783864a525203146d96bf36
Nestor Ramirez <nesthor2706@gmail.com>
2026-04-08 10:09:14 -0400

    estructura base del juego con canvas

---
+++ game.js
@@ -0,0 +1,12 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+function gameLoop() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
